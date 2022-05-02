8bbebd0e05362c1a6842737fcc90485b3dc7161b
David Rolfe <drolfe@voltdb.com>
2022-05-02 12:07:10 +0100

    slowed down Send

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -8,28 +8,28 @@ QUERYSECONDS=20
 
 PCO=0
 LOC=0
+DT=`date '+%Y%m%d_%H%M'`
 
-cd ../jars 
+cd ../jars
 
 kill `ps -deaf | grep java | grep voltdb-edge | awk '{ print $2 }'` 2> /dev/null
 sleep 5
 
 while
-	[ $LOC -lt $LOCATIONS ]
+        [ $LOC -lt $LOCATIONS ]
 do
-	java -jar voltdb-edge-devices.jar `cat $HOME/.vdbhostnames`  ${DURATION} ${LOC} > location_${LOC}.lst  & 
-	LOC=`expr $LOC + 1`
+        java -jar voltdb-edge-devices.jar `cat $HOME/.vdbhostnames`  ${DURATION} ${LOC} > location_${LOC}_${DT}_${POWERCOS}_${LOCATIONS}_${TPS}_${METERS}.lst  &
+        LOC=`expr $LOC + 1`
 done
 
 sleep 5
 
 while
-	[ $PCO -lt $POWERCOS ]
+        [ $PCO -lt $POWERCOS ]
 do
-	java -jar voltdb-edge-powerco.jar `cat $HOME/.vdbhostnames`  ${TPS} ${DURATION} ${METERS} ${QUERYSECONDS}  $PCO > powerco_${PCO}.lst  &
-	PCO=`expr $PCO + 1`
+        java -jar voltdb-edge-powerco.jar `cat $HOME/.vdbhostnames`  ${TPS} ${DURATION} ${METERS} ${QUERYSECONDS}  $PCO > powerco_${PCO}_${DT}_${POWERCOS}_${LOCATIONS}_${TPS}_${METERS}.lst  &
+        PCO=`expr $PCO + 1`
 done
 
 
 wait
-
