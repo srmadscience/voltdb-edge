a31b410f0a6785fc76b54d3d270f82a177802679
David Rolfe <drolfe@voltdb.com>
2022-05-05 08:58:13 +0100

    Added missing copyright notices

--- testSrc/org/voltse/test/TestSendDownstreamWithVolt.java
+++ testSrc/org/voltse/test/TestSendDownstreamWithVolt.java
@@ -1,3 +1,26 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2022 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package org.voltse.test;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -78,8 +101,9 @@ class TestSendDownstreamWithVolt {
             m.setDeviceId(GENERIC_DEVICE_ID);
             m.setExternallMessageId(GENERIC_EXTERNAL_MESSAGE_ID);
 
+            final String message = Base64.getEncoder().encodeToString(m.asJson(g).getBytes());
             ClientResponse cr = c.callProcedure("SendMessageDownstream", m.getDeviceId(), TEST_OWNER,
-                    Base64.getEncoder().encodeToString(m.asJson(g).getBytes()));
+                    message);
 
             if (cr.getStatus() != ClientResponse.SUCCESS) {
                 fail(cr.getStatusString());
